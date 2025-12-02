370add961623acc3b488a5e1b9636901346d98fd
achref messai <achref.messai@insat.ucar.tn>
2025-12-02 14:30:05 +0100

    Initial commit: Infrastructure IaC complète

---
+++ main.tf
@@ -0,0 +1,63 @@
+# --- Configuration Terraform et Provider ---
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1" # Spécifie le provider Docker
+    }
+  }
+}
+
+provider "docker" {}
+
+# --- 1. Ressource : Base de Données PostgreSQL ---
+
+# Télécharge l'image PostgreSQL depuis Docker Hub
+resource "docker_image" "postgres_image" {
+  name         = "postgres:latest"
+  keep_locally = true
+}
+
+# Crée et configure le conteneur PostgreSQL
+resource "docker_container" "db_container" {
+  name  = "tp-db-postgres"
+  image = docker_image.postgres_image.image_id
+
+  ports {
+    internal = 5432
+    external = var.db_port_external # Utilise la variable pour éviter les conflits
+  }
+
+  # Configuration de la DB via les variables d'environnement
+  env = [
+    "POSTGRES_USER=${var.db_user}",
+    "POSTGRES_PASSWORD=${var.db_password}",
+    "POSTGRES_DB=${var.db_name}",
+  ]
+}
+
+# --- 2. Ressource : Application Web Nginx ---
+
+# Utilise l'image de l'application déjà construite
+resource "docker_image" "app_image" {
+  name         = "tp-web-app:latest"
+  keep_locally = true
+}
+
+# Crée le conteneur de l'application web
+resource "docker_container" "app_container" {
+  name  = "tp-app-web"
+  image = docker_image.app_image.image_id
+
+  # Dépendance explicite : la DB doit être prête avant l'Application
+  depends_on = [
+    docker_container.db_container
+  ]
+
+  # Mappage du port 80 interne au port externe défini dans variables.tf (par défaut 8080)
+  ports {
+    internal = 80
+    external = var.app_port_external
+  }
+}
